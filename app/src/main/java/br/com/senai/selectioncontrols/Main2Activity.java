dba15284da011cc5ef9174795eacf58c5475d75e
gui12pereira <guipereirasantos2002@gmail.com>
2019-09-30 13:11:19 -0300

    Update.

--- app/src/main/java/br/com/senai/selectioncontrols/Main2Activity.java
+++ app/src/main/java/br/com/senai/selectioncontrols/Main2Activity.java
@@ -1,16 +1,24 @@
 package br.com.senai.selectioncontrols;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageButton;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private AppCompatImageButton imgButton;
-    private AppCompatImageButton galeria;
+    private ImageButton imageButton;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
 
     @Override
@@ -20,23 +28,49 @@ public class Main2Activity extends AppCompatActivity {
 
         setTitle("Activity 2");
 
-        imgButton = findViewById(R.id.img_button_2);
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mudarActivity3 = new Intent(Main2Activity.this, Main3Activity.class);
-                startActivity(mudarActivity3);
-            }
-        });
+        imageButton = findViewById(R.id.bt_foto);
+        imageView = findViewById(R.id.imageView);
 
-        galeria = findViewById(R.id.img_fotos);
-        galeria.setOnClickListener(new View.OnClickListener(){
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent foto = new Intent();
-                foto.setType("image/*");
-                foto.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(foto, 0);
+                foto.setType("image/*");foto.setAction(Intent.ACTION_PICK);
+                startActivityForResult(foto, 1232);
+            }
+        });
+
+        //galeria = findViewById(R.id.img_fotos);
+        //galeria.setOnClickListener(new View.OnClickListener(){
+        //@Override
+        //public void onClick(View view) {
+        //Intent foto = new Intent();
+        //foto.setType("image/*");
+        //foto.setAction(Intent.ACTION_GET_CONTENT);
+        //startActivityForResult(foto, 0);
+        //}
+        //});
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("Valor do resultCode = ", String.valueOf(resultCode));
+
+        if (requestCode == 1232){
+            Uri path = data.getData();
+
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
+
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+
+                e.printStackTrace();
             }
         }
     }
